9f7ba40f9a1704b2c821b03f86228bf76f0bea2e
tayane <tayane.soares@atta.com.vc>
2021-08-04 17:55:32 -0300

    aplicação de tarefas

--- src/store/index.js
+++ src/store/index.js
@@ -5,9 +5,37 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tarefas: [
+        {
+          id: 1,
+          titulo: 'Ir ao mercado',
+          concluido: 'false',
+        },
+        {
+          id: 2,
+          titulo: 'Comprar leite',
+          concluido: 'false',
+        }
+    ],
   },
+  //metodo que pode fazer algo com estado
   mutations: {
+    adicionaTarefa(state, titulo) {
+      if(titulo) {
+      state.tarefas.push({
+        id: new Date().getTime(),
+        titulo,
+        concluido: false,
+      })
+    }
   },
+    removeTarefa(state, id)  {
+     state.tarefas = state.tarefas.filter(tarefa => tarefa.id !== id)
+
+  },
+  },
+  
+
   actions: {
   },
   modules: {
---
+++ src/views/Tarefas.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <div>
+      <v-col
+         cols="12"
+       >
+       <v-text-field
+          v-model="campoInput"
+          label="Qual a sua tarefa?"
+          outlined
+          clearable
+          @keyup.enter="handleAddTarefa"
+         >
+        </v-text-field>
+      </v-col>
+    <v-list
+      flat
+      subheader
+    >
+
+     <v-list-item-group
+      multiple
+      active-class=""
+    >
+      <div
+       v-for="tarefa, index in $store.state.tarefas"
+       :key="index"
+      >
+        <Tarefa
+         :tarefa="tarefa" />
+      </div>
+
+      <Tarefa />
+      <Tarefa />
+
+
+       
+      </v-list-item-group>
+    </v-list>
+ </div>
+
+  </div>
+</template>
+ 
+<script>
+import Tarefa from '../components/tarefas/Tarefa.vue'
+  export default {
+    name: 'Home',
+
+    components: {
+      Tarefa
+    },
+    data () {
+      return {
+        campoInput: null,
+      }
+    },
+    methods: {
+     handleAddTarefa () {
+       //chamando a mutations
+       this.$store.commit('adicionaTarefa', this.campoInput)
+       this.campoInput = null
+     }
+    }
+  }
+</script>
